b7eb41661adb255d190f33fd4f214f9e35466415
JedediahKKH <kimhai.koh@connect.qut.edu.au>
2019-09-08 19:36:02 +1000

    Added DroneObject
    Added some pseudo-code for Drone Object

--- GetSimParameters.m
+++ GetSimParameters.m
@@ -7,7 +7,7 @@ function [parameters]= GetSimParameters()
 
 max_ascend_speed=5; %m/s
 max_descend_speed=3; %m/s
-max_speed=72000/3600; %m/s
+max_speed=25000/3600; %m/s
 max_flight_distance=18000; %m
 max_flight_time=31*3600; %s at 25kph
 
